a5aa8ebddaa24af5039e4c43ced181a54780627f
Maddi-Harish <harish3333reddy@gmail.com>
2024-08-17 19:00:17 +0530

    Changed into Https

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.use("/images", express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `https://adminecomapp.onrender.com${port}/images/${req.file.filename}`
+        image_url: `https://adminecomapp.onrender.com/images/${req.file.filename}`
     })
 })
 
