c7e348e07b3f5bf03b3ae01cf0ea83167ae814c5
Maddi-Harish <harish3333reddy@gmail.com>
2024-08-20 19:25:01 +0530

    Urls Updated in Origin

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const bcrypt = require("bcryptjs");
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://adminecomapp.onrender.com', 'https://ecomzone.netlify.app/']
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://adminecomapp.onrender.com', 'https://ecomzone.netlify.app/']
 }));
 
 // Database Connection With MangoDB 
